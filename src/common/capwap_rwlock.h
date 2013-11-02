769d0f414ec680c9d0802750dfdbc6451c4e5d02
vemax78 <devnull@localhost>
2013-11-02 19:04:56 +0100

    Add support for rwlock

---
+++ src/common/capwap_rwlock.h
@@ -0,0 +1,20 @@
+#ifndef __CAPWAP_RWLOCK_HEADER__
+#define __CAPWAP_RWLOCK_HEADER__
+
+#ifdef CAPWAP_MULTITHREADING_ENABLE
+
+#include <pthread.h>
+
+typedef struct {
+	pthread_rwlock_t rwlock;
+} capwap_rwlock_t;
+
+int capwap_rwlock_init(capwap_rwlock_t* lock);
+void capwap_rwlock_destroy(capwap_rwlock_t* lock);
+void capwap_rwlock_rdlock(capwap_rwlock_t* lock);
+void capwap_rwlock_wrlock(capwap_rwlock_t* lock);
+void capwap_rwlock_exit(capwap_rwlock_t* lock);
+
+#endif /* CAPWAP_MULTITHREADING_ENABLE */
+
+#endif /* __CAPWAP_RWLOCK_HEADER__ */
